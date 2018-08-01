4096d11e0c0c7eb88923b783d94344d4d02b3be2
Dan Hintz <dhintz@live.com>
2018-08-01 02:19:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const app = "I don't do much."
 
+var dodger=document.getElementById('#dodger');
+
 function moveDodgerRight() {
   document.addEventListener('keydown',function(e) {
-    if(e.which===)
-  })
+    if(e.which===39) {
+      var rightNum=dodger.style.right.replace('px','');
+      var right=parseInt(rightNum,10);
+      if(right>0) {
+        dodger.style.right=`${right-1}px`;
+      }
+    }
+  });
 }
